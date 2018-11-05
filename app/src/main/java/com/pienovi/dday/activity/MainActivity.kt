02c28879a50d5130cee345f085c107cf4cfeb58a
Carlos Pienovi <carlos@decemberlabs.com>
2018-11-05 11:24:35 -0300

    add Firebase

---
+++ app/src/main/java/com/pienovi/dday/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.pienovi.dday.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.pienovi.dday.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
