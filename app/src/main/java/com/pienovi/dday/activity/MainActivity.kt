5756d21d16f31b724666df3e80d2d377e788cf0a
Carlos Pienovi <carlos@decemberlabs.com>
2018-11-16 15:21:44 -0300

    draw in overlay ImageView and avoid multiple bitmap allocation

--- app/src/main/java/com/pienovi/dday/activity/MainActivity.kt
+++ app/src/main/java/com/pienovi/dday/activity/MainActivity.kt
@@ -6,10 +6,8 @@ import android.support.v7.app.AppCompatActivity
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
-import com.google.firebase.ml.vision.face.FirebaseVisionFace
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceContour
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+import com.google.firebase.ml.vision.common.FirebaseVisionPoint
+import com.google.firebase.ml.vision.face.*
 import com.otaliastudios.cameraview.Facing
 import com.otaliastudios.cameraview.Frame
 import com.otaliastudios.cameraview.Gesture
@@ -22,27 +20,65 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
 
-        private const val RADIUS = 2f
+        private const val RADIUS = 3f
+        private const val STROKE_WIDTH = 2f
+        private const val COLOR_DOT = Color.WHITE
+        private const val COLOR_LINE = Color.GREEN
+        private const val COLOR_MOUTH = Color.RED
+        private const val COLOR_EYE_IRIS = Color.CYAN
+        private const val COLOR_EYE_LID = Color.YELLOW
+        private const val MIN_EYE_OPEN_PROBABILITY = 0.5f
+        private const val EYE_RADIUS_PROPORTION = 0.45f
+        private const val IRIS_RADIUS_PROPORTION = EYE_RADIUS_PROPORTION / 2.0f
 
     }
 
     private var processing = false
     private var bitmap: Bitmap? = null
     private var canvas: Canvas? = null
+
     private val dotPaint = Paint()
     private val linePaint = Paint()
+    private val mouthPaint = Paint()
+
+    private var eyeWhitesPaint = Paint()
+    private var eyeIrisPaint = Paint()
+    private var eyeOutlinePaint = Paint()
+    private var eyeLidPaint = Paint()
+
     private lateinit var faceDetector: FirebaseVisionFaceDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        dotPaint.color = Color.RED
+        dotPaint.color = COLOR_DOT
         dotPaint.style = Paint.Style.FILL
-        dotPaint.strokeWidth = 2f
-        linePaint.color = Color.GREEN
+        dotPaint.strokeWidth = STROKE_WIDTH
+
+        linePaint.color = COLOR_LINE
         linePaint.style = Paint.Style.STROKE
-        linePaint.strokeWidth = 1f
+        linePaint.strokeWidth = STROKE_WIDTH
+
+        mouthPaint.color = COLOR_MOUTH
+        mouthPaint.style = Paint.Style.FILL
+
+        eyeWhitesPaint = Paint()
+        eyeWhitesPaint.color = Color.WHITE
+        eyeWhitesPaint.style = Paint.Style.FILL
+
+        eyeLidPaint = Paint()
+        eyeLidPaint.color = COLOR_EYE_LID
+        eyeLidPaint.style = Paint.Style.FILL
+
+        eyeIrisPaint = Paint()
+        eyeIrisPaint.color = COLOR_EYE_IRIS
+        eyeIrisPaint.style = Paint.Style.FILL
+
+        eyeOutlinePaint = Paint()
+        eyeOutlinePaint.color = Color.BLACK
+        eyeOutlinePaint.style = Paint.Style.STROKE
+        eyeOutlinePaint.strokeWidth = 5f
 
         cameraView.setLifecycleOwner(this)
         cameraView.mapGesture(Gesture.PINCH, GestureAction.ZOOM)
@@ -62,8 +98,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         val options = FirebaseVisionFaceDetectorOptions.Builder()
+            .setPerformanceMode(FirebaseVisionFaceDetectorOptions.FAST)
             .setContourMode(FirebaseVisionFaceDetectorOptions.ALL_CONTOURS)
             .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+            .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
             .build()
         faceDetector = FirebaseVision.getInstance().getVisionFaceDetector(options)
     }
@@ -102,7 +140,18 @@ class MainActivity : AppCompatActivity() {
                     canvas!!.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
                 }
 
-                drawFaces(canvas!!, it)
+                if (contoursCheckBox.isChecked) {
+                    drawContours(canvas!!, it)
+                }
+                if (mouthCheckBox.isChecked) {
+                    drawMouth(canvas!!, it)
+                }
+                if (eyesCheckBox.isChecked) {
+                    drawEyes(canvas!!, it)
+                }
+                if (contoursCheckBox.isChecked || mouthCheckBox.isChecked || eyesCheckBox.isChecked) {
+                    updateImageView()
+                }
             }
             .addOnFailureListener {
                 processing = false
@@ -110,8 +159,21 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private fun drawFaces(canvas: Canvas, faces: List<FirebaseVisionFace>) {
+    private fun updateImageView() {
+        if (cameraView.facing == Facing.FRONT) {
+            val matrix = Matrix()
+            matrix.preScale(-1F, 1F)
+            val flippedBitmap =
+                Bitmap.createBitmap(bitmap!!, 0, 0, bitmap!!.width, bitmap!!.height, matrix, true)
+            imageView.setImageBitmap(flippedBitmap)
+        } else {
+            imageView.setImageBitmap(bitmap)
+        }
+    }
+
+    private fun drawContours(canvas: Canvas, faces: List<FirebaseVisionFace>) {
         for (face in faces) {
+
             val faceContours = face.getContour(FirebaseVisionFaceContour.FACE).points
             for ((i, contour) in faceContours.withIndex()) {
                 if (i != faceContours.lastIndex)
@@ -294,17 +356,92 @@ class MainActivity : AppCompatActivity() {
                     )
                 canvas.drawCircle(contour.x, contour.y, RADIUS, dotPaint)
             }
-
-            if (cameraView.facing == Facing.FRONT) {
-                val matrix = Matrix()
-                matrix.preScale(-1F, 1F)
-                val flippedBitmap =
-                    Bitmap.createBitmap(bitmap!!, 0, 0, bitmap!!.width, bitmap!!.height, matrix, true)
-                imageView.setImageBitmap(flippedBitmap)
-            } else {
-                imageView.setImageBitmap(bitmap)
-            }
         }
+    }
+
+    private fun drawMouth(canvas: Canvas, faces: List<FirebaseVisionFace>) {
+        for (face in faces) {
+            val upperLipTopContours = face.getContour(FirebaseVisionFaceContour.UPPER_LIP_TOP).points
+            val upperLipBottomContours = face.getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).points
+            drawLip(canvas, upperLipTopContours, upperLipBottomContours)
+
+            val lowerLipTopContours = face.getContour(FirebaseVisionFaceContour.LOWER_LIP_TOP).points
+            val lowerLipBottomContours = face.getContour(FirebaseVisionFaceContour.LOWER_LIP_BOTTOM).points
+            drawLip(canvas, lowerLipTopContours, lowerLipBottomContours)
+        }
+    }
+
+    private fun drawLip(
+        canvas: Canvas,
+        topContours: List<FirebaseVisionPoint>,
+        bottomContours: List<FirebaseVisionPoint>
+    ) {
+        if (topContours.isEmpty() || bottomContours.isEmpty()) {
+            return
+        }
+
+        val pathUpperLip = Path()
+
+        val firstUpperX = topContours[0].x
+        val firstUpperY = topContours[0].y
+        pathUpperLip.moveTo(firstUpperX, firstUpperY)
+
+        for (contour in topContours) {
+            pathUpperLip.lineTo(contour.x, contour.y)
+        }
+
+        for (i in (bottomContours.size - 1) downTo 0) {
+            val contour = bottomContours[i]
+            pathUpperLip.lineTo(contour.x, contour.y)
+        }
+
+        pathUpperLip.close()
+        canvas.drawPath(pathUpperLip, mouthPaint)
+    }
+
+    private fun drawEyes(canvas: Canvas, faces: List<FirebaseVisionFace>) {
+        for (face in faces) {
+            val leftEye = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EYE)
+            val rightEye = face.getLandmark(FirebaseVisionFaceLandmark.RIGHT_EYE)
+
+            if (leftEye == null || rightEye == null) {
+                return
+            }
+
+            val detectLeftPosition = leftEye.position
+            val detectRightPosition = rightEye.position
+            val leftPosition = PointF(detectLeftPosition.x, detectLeftPosition.y)
+            val rightPosition = PointF(detectRightPosition.x, detectRightPosition.y)
+            val leftOpen = face.leftEyeOpenProbability > MIN_EYE_OPEN_PROBABILITY
+            val rightOpen = face.rightEyeOpenProbability > MIN_EYE_OPEN_PROBABILITY
+
+            val distance = Math.sqrt(
+                Math.pow(rightPosition.x.toDouble() - leftPosition.x, 2.0)
+                        + Math.pow(rightPosition.y.toDouble() - leftPosition.y, 2.0)
+            )
+            val eyeRadius = EYE_RADIUS_PROPORTION * distance
+            val irisRadius = IRIS_RADIUS_PROPORTION * distance
+
+            drawEye(canvas, leftPosition, eyeRadius.toFloat(), leftPosition, irisRadius.toFloat(), leftOpen)
+            drawEye(canvas, rightPosition, eyeRadius.toFloat(), rightPosition, irisRadius.toFloat(), rightOpen)
+        }
+    }
+
+    private fun drawEye(
+        canvas: Canvas, eyePosition: PointF, eyeRadius: Float,
+        irisPosition: PointF, irisRadius: Float, isOpen: Boolean
+    ) {
+        if (isOpen) {
+            canvas.drawCircle(eyePosition.x, eyePosition.y, eyeRadius, eyeWhitesPaint)
+            canvas.drawCircle(irisPosition.x, irisPosition.y, irisRadius, eyeIrisPaint)
+        } else {
+            canvas.drawCircle(eyePosition.x, eyePosition.y, eyeRadius, eyeLidPaint)
+            val y = eyePosition.y
+            val start = eyePosition.x - eyeRadius
+            val end = eyePosition.x + eyeRadius
+            canvas.drawLine(start, y, end, y, eyeOutlinePaint)
+        }
+        canvas.drawCircle(eyePosition.x, eyePosition.y, eyeRadius, eyeOutlinePaint)
     }
 
 }
